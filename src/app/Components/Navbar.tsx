55eec0ab191282ddb6bfcb3f49ccf4bed51ac92c
arhum khan <arhum@berkeley.edu>
2024-02-02 23:14:42 -0800

    added code

---
+++ src/app/Components/Navbar.tsx
@@ -0,0 +1,19 @@
+
+import Image from "next/image"
+import Link from "next/link"
+
+export default function Navbar() {
+    return (
+        <nav className="text-3xl text-black w-full font-salvatore ">
+            <div className="flex justify-between items-center h-full w-full pl-4 ">
+                <div className="flex pl-4 pt-2 items-center  ">
+                    <Image src="/calhacks.png" alt="calhackslogo" width="100" height="100" />
+                    <Link href="/" passHref>
+                        <h1 className="">CalHacks 11.0 Registration</h1>
+                    </Link>
+                </div>
+
+            </div>
+        </nav>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,164 @@
+"use client"
 import Image from "next/image";
+import { ChangeEventHandler, useState } from "react";
+import Navbar from "./Components/Navbar";
 
 export default function Home() {
+
+
+  /* const [formData, setFormData] = useState({
+    fullName: null,
+    university: null,
+    major: null,
+    gradDate: null,
+    gender: null,
+    country: null,
+    essay: null,
+  }) */
+  const essayPrompt = `
+  Why are you interested in attending Cal Hacks 11.0? What are you interested in
+  building? (1000 character maximum)
+  `
+  const [formData, setFormData] = useState<any>({
+    fullName: {
+      data: null, label: "Full Name", var: "fullName", type: "text", placeholder: "John Doe", id: "fullName", name: "fullName"
+    },
+    university: {
+      data: null, label: "University", var: "university", type: "text", placeholder: "University of California - Berkeley", id: "university", name: "university"
+    },
+    major: {
+      data: null, label: "Major", var: "major", type: "text", placeholder: "Computer Science", id: "major", name: "major"
+    },
+    gradDate: {
+      data: null, label: "Graduation Date", var: "gradDate", type: "date", placeholder: "", id: "gradDate", name: "gradDate"
+    },
+    gender: {
+      data: null, label: "Gender", var: "gender", type: "text", placeholder: "", id: "gender", name: "gender"
+    },
+    country: {
+      data: null, label: "Country of Residence", var: "country", placeholder: "", id: "country", name: "country"
+    },
+    essay: {
+      data: null, label: "The Essay", var: "essay", placeholder: ". . .", id: "essay", name: "essay"
+    },
+  })
+
+  const [submitted, setSubmitted] = useState(false)
+  const [errorMessage, setErrorMessage] = useState<string[]>([]);
+  const [ageCheck, setAgeCheck] = useState(false)
+  const [error, setError] = useState(false)
+
+
+
+
+  const handleChange: React.ChangeEvent<HTMLSelectElement> | ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> = (e) => {
+    const { name, value } = e.target;
+
+    setFormData((prevFormData: any) => ({
+      ...prevFormData,
+      [name]: {
+        ...prevFormData[name],
+        data: value
+      },
+    }));
+
+  }
+
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    setSubmitted(true)
+    setError(false)
+
+    const tempErrors: string[] = []
+
+    Object.keys(formData).forEach((key) => {
+      if (formData[key as keyof typeof formData].data === null) {
+        tempErrors.push(`${formData[key].label}  is required *`)
+      }
+    })
+    /* errorMessage.push(`${nullFields.join(', ')} are required fields`); */
+
+    if (!ageCheck) {
+      tempErrors.push("You must be 18+ to participate in the hackathon. *")
+    }
+
+    if (tempErrors.length > 0) {
+      setError(true)
+    }
+    else {
+      setError(false)
+    }
+
+    if (formData.essay.data && formData.essay.data.length > 1000) {
+      tempErrors.push("Your response cannot be more than 1000 characters")
+    }
+
+    setErrorMessage(tempErrors)
+    console.log(errorMessage)
+
+  }
+
+
+  console.log(formData)
+  console.log(error)
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="bg-gradient-to-br from-white to-sky-100">
+      <Navbar />
+      <div className="flex pt-4 justify-center items-start">
+        <form
+          className="flex flex-col gap-4  px-24 font-salvatore"
+          onSubmit={(e) => {
+            setErrorMessage([]);
+            handleSubmit(e);
+          }}
+        >
+          {Object.keys(formData).map((key) => {
+            const field = formData[key];
+            if (key === 'essay') { return null }
+            if (field.type !== 'checkbox') {
+              return (
+                <>
+                  <label> {field.label} </label>
+                  <input
+                    className="border hover:border-2 transition-all duration-100 border-black rounded-lg bg-blue-200 w-96 px-2 py-2"
+                    type={field.type}
+                    placeholder={field.placeholder}
+                    id={field.id}
+                    name={field.name}
+                    value={field.data}
+                    onChange={handleChange}
+                  />
+                </>
+              );
+            }
+
+          })}
+
+          <label> {essayPrompt}</label>
+          <textarea
+            className="border hover:border-2 transition-all duration-100 border-black rounded-lg  w-3/4 h-80 px-2 py-2"
+            id="essay"
+            name="essay"
+            value={formData.essay.data as any}
+            onChange={handleChange}
+            rows={6}
+          />
+          <label>
+            <input
+              type="checkbox"
+              onChange={() => setAgeCheck(!ageCheck)}
+            /> Check the box if you are 18+
+          </label>
+
+          {error && errorMessage.map((message, index) => <p className="text-red-500">{message}</p>)}
+
+
+          <button className="border rounded-xl bg-blue-400 w-20 p-2 text-white" type="submit"> Submit</button>
+        </form>
       </div>
+    </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+  )
 }
